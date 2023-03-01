a6cef80e45f1535e4e1ac479a14e826e3ba11313
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-03-01 10:01:09 +0600

    method

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
     data(){
         return{
-             name: 'Mynul',
+             name: 'test 1',
              link: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        numberTest(){
+            const number = Math.random()
+            if (number < 0.5){
+                return 'working';
+            }else{
+                return 'not working';
+            }
+        }
     }
 });
 
