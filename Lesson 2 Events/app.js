24f9f7b4568dffa3bb91d5bea5ae520c9c81fc46
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-03-05 10:36:39 +0600

    calling methods with events

--- Lesson 2 Events/app.js
+++ Lesson 2 Events/app.js
@@ -4,5 +4,14 @@ const app = Vue.createApp({
             counter: 0,
         };
     },
+    methods: {
+        add(){
+            this.counter=this.counter+1
+        },
+        sub(){
+            this.counter = this.counter -1
+        }
+    }
+
 });
 app.mount('#event')
