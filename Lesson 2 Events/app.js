8001a04b1a927ac052a530569d81a9b92398acdb
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-03-06 10:23:24 +0600

    Native Event Object

--- Lesson 2 Events/app.js
+++ Lesson 2 Events/app.js
@@ -2,9 +2,17 @@ const app = Vue.createApp({
     data(){
         return{
             counter: 0,
+            name: ''
         };
     },
     methods: {
+        setName(event){
+            this.name = event.target.value;
+            // passing whole string
+            console.log(event.target.value)
+            //passing one by one string 
+            console.log(event.data)
+        },
         add(num){
             this.counter=this.counter+num;
         },
