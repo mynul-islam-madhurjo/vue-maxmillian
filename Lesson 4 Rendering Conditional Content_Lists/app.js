788c5d391186f656ba070d1eb19250db0f224b3e
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-04-09 08:43:04 +0600

    v - if conditional rendering

--- Lesson 4 Rendering Conditional Content_Lists/app.js
+++ Lesson 4 Rendering Conditional Content_Lists/app.js
@@ -2,9 +2,15 @@ const app = Vue.createApp({
 
   data() {
     return { 
-      goals: [] 
+      goals: [],
+      enteredGoals: '' 
     };
   },
+  methods: {
+    addGoal(){
+      this.goals.push(this.enteredGoals)
+    }
+  }
 });
 
 app.mount('#user-goals');
