885206946a02976b9871288d265ac776fbf56f50
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-04-10 09:49:08 +0600

    attack mehtods

---
+++ Lesson 5 Project/app.js
@@ -0,0 +1,21 @@
+ function randomValue(max,min){
+    return Math.floor(Math.random()*(max-min)+5);
+ }
+
+ const app = Vue.createApp({
+    data() {
+        return{
+            monsterHealth: 100,
+            playerHealth: 100
+        };
+    },
+    methods: {
+        attackMonster(){
+            this.monsterHealth -= randomValue(10,5);
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            this.playerHealth -= randomValue(15,5);
+        }
+    }
+ });
