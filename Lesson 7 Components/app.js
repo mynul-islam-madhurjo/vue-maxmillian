b4962b374326dc0b79eef22804266d0a6c3a21a4
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-04-16 10:05:04 +0600

    component introduction

--- Lesson 7 Components/app.js
+++ Lesson 7 Components/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data(){
         return {
-            dataVisible: false,
             friends: [{
                 id: 'manuel',
                 name: 'Manuel Lorenz',
@@ -17,14 +16,37 @@ const app = Vue.createApp({
         ],
 
         }
+    }
+});
+
+//custom html element
+app.component('friend-contact', {
+    template: `
+    <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ dataVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="dataVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+    `,
+    data(){
+        return {
+            friend: {
+                id: 'julie',
+                name: 'Julie Jones',
+                phone: '09876 543 221',
+                email: 'julie@localhost.com'
+            },
+            dataVisible: false,
+        }
     },
     methods: {
         toggleDetails(){
             this.dataVisible = !this.dataVisible; 
         }
     }
-
-
 });
 
 app.mount('#app')
