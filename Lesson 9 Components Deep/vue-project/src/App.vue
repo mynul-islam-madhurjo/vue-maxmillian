d2e08d2335aac59dde0104fef12c777aaab9291f
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-05-10 10:46:54 +0600

    Adding a child component inside a child

---
+++ Lesson 9 Components Deep/vue-project/src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import BadgeList from './components/BadgeList.vue'
+import BaseBadge from './components/BaseBadge.vue'
+import TheHeader from './components/TheHeader.vue'
+import UserInfo from './components/UserInfo.vue'
+
+// Any variables or functions defined here will be available in the template
+
+</script>
+
+<template>
+
+<div>
+
+
+<TheHeader/>
+<BadgeList/>
+<UserInfo
+  :full-name="activeUser.name"
+  :info-text="activeUser.description"
+  :role="activeUser.role"
+/>
+
+</div>
+</template>  
+
+<script>
+// If you need to define variables or functions outside of the <script setup> section,
+// you can do so in a separate <script> block
+export default { 
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+}
+
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ Lesson 9 Components Deep/vue-project/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import BaseBadge from './components/BaseBadge.vue'
+import './assets/main.css'
+
+createApp(App).mount('#app')
+
+
+//Globally declared components
+// const app = createApp(App);
+// app.component('base-badge', BaseBadge);
