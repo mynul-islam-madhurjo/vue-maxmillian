fef08be0af2c5d7a8ed9b7c8da439bb1d422171a
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-05-14 15:58:19 +0600

    minor update

--- Lesson 9 Components Deep/vue-project/src/components/ManageGoals.vue
+++ Lesson 9 Components Deep/vue-project/src/components/ManageGoals.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>
+    <div v-if="!goalEmpty">
         <h1>
             Goals
         </h1>
         <input v-model="storedGoal" type="text" />
-        <div v-if="goalEmpty">
+        <button @click="submitGoal">Submit</button>
+
+        <div :class="['success-message', { 'fade-out': !showSuccessMessage }]">
+            Submitted successfully!
+        </div>
+    </div>
+
+    <div v-if="goalEmpty">
             <!-- Not nested anymore inside the body tag -->
             <teleport to='body'>
                 <error-dialogue >
@@ -15,9 +22,9 @@
                 </error-dialogue>
             </teleport>
         </div>
-        <button @click="submitGoal">Submit</button>
-
-    </div>
+        <div :class="['fail-message', { 'fade-out': !showFailMessage }]">
+            Not Submitted!
+        </div>
 </template>
 
 <script lang="ts">
@@ -32,6 +39,8 @@ import ErrorDialogue from './ErrorDialogue.vue'
             return {
                 storedGoal: '',
                 goalEmpty: false,
+                showSuccessMessage: false,
+                showFailMessage: false,
             }
         },
         methods : {
@@ -40,8 +49,18 @@ import ErrorDialogue from './ErrorDialogue.vue'
                 if (enteredGoal){
                     console.log(enteredGoal);
                     this.$emit('add-goal', enteredGoal)
+                    // Show success message for a few seconds
+                    this.showSuccessMessage = true;
+                    setTimeout(() => {
+                        this.showSuccessMessage = false;
+                    }, 1000);// Adjust the duration as needed (e.g., 1000ms = 1 seconds)
                 }else{
                     this.goalEmpty = true;
+                    // Show success message for a few seconds
+                    this.showFailMessage = true;
+                    setTimeout(() => {
+                        this.showFailMessage = false;
+                    }, 1000)
                 }
             },
             resetGoal(){
@@ -51,6 +70,31 @@ import ErrorDialogue from './ErrorDialogue.vue'
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.success-message {
+  background-color: green;
+  color: white;
+  padding: 10px;
+  margin-top: 10px;
+  opacity: 1;
+  transition: opacity 0.5s; /* Add a transition property for the opacity */
+}
+
+.success-message.fade-out {
+  opacity: 0;
+}
+.fail-message {
+  background-color: red;
+  color: white;
+  padding: 10px;
+  margin-top: 10px;
+  opacity: 1;
+  transition: opacity 0.5s; /* Add a transition property for the opacity */
+}
+
+.fail-message.fade-out {
+  opacity: 0;
+}
+
 
 </style>
