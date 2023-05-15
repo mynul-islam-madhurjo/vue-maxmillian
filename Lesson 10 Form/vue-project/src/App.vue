21f05784b26f9ebb2a7876bb1745feca59981656
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-05-15 10:26:53 +0600

    working with forms in vue

---
+++ Lesson 10 Form/vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+
+// Any variables or functions defined here will be available in the template
+
+</script>
+
+<template>
+
+    <h1>Working</h1>
+
+
+ 
+</template>  
+
+<script>
+
+export default { 
+
+}
+
+</script>
+
+ 
+<style>
+</style>
